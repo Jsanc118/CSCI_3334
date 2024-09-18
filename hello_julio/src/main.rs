3cb9e0091b2e9baadc102da7078947f9642b094a
Jsanc118 <122334643+Jsanc118@users.noreply.github.com>
2024-09-18 00:32:58 +0000

    Assignment completed

--- hello_julio/src/main.rs
+++ hello_julio/src/main.rs
@@ -1,9 +1,28 @@
-fn main() {
-    let mut x: i32 = 5;
-    println!("The value of x is: {}", x);
-    
-    // Uncommenting the next line will result in a compilation error
-     x += 6; // error: cannot assign twice to immutable variable `x`
-     println!("The value of x is: {}", x);
 
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + 32.0
+}
+
+fn main() {
+    
+    let mut temp_f = 32.0;
+
+    
+    let temp_c = fahrenheit_to_celsius(temp_f);
+    println!("{:.2}°F is {:.2}°C", temp_f, temp_c);
+
+    
+    let back_to_f = celsius_to_fahrenheit(temp_c);
+    println!("{:.2}°C is {:.2}°F", temp_c, back_to_f);
+
+    for _ in 1..=5 {
+        temp_f += 1.0;
+        let temp_c = fahrenheit_to_celsius(temp_f);
+        let back_to_f = celsius_to_fahrenheit(temp_c);
+        println!("{:.2}°F is {:.2}°C | {:.2}°C is {:.2}°F", temp_f, temp_c, temp_c, back_to_f);
+    }
 }
