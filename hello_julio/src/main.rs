4739737a8c5bcacddfad61b2406d65e99ebe509b
Jsanc118 <122334643+Jsanc118@users.noreply.github.com>
2024-09-18 00:30:26 +0000

    Assignment 1 Completed

--- hello_julio/src/main.rs
+++ hello_julio/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, Julio!");
+    let mut x: i32 = 5;
+    println!("The value of x is: {}", x);
+    
+    // Uncommenting the next line will result in a compilation error
+     x += 6; // error: cannot assign twice to immutable variable `x`
+     println!("The value of x is: {}", x);
+
 }
